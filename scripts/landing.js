fd3c3955ee6211aa6a376e6e957ed42d414cbe5c
Travis Rodgers <rodgtr1@gmail.com>
2017-01-02 17:21:37 -0500

    checkpoint8Animation2: Complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,14 @@
-                    var animatePoints = function() {
-                    
-                    var points = document.getElementsByClassName('point');
-                    
-                    var revealFirstPoint = function() {
-                        points[0].style.opacity = 1;
-                        points[0].style.transform = "scaleX(1) translateY(0)";
-                        points[0].style.msTransform = "scaleX(1) translateY(0)";
-                        points[0].style.WebkitTransform = "scale(1) translate(0)";
-                    };
-                    
-                    var revealSecondPoint = function() {
-                        points[1].style.opacity = 1;
-                        points[1].style.transform = "scaleX(1) translateY(0)";
-                        points[1].style.msTransform = "scaleX(1) translateY(0)";
-                        points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-                    };
+  var animatePoints = function() {
  
-                    var revealThirdPoint = function() {
-                        points[2].style.opacity = 1;
-                        points[2].style.transform = "scaleX(1) translateY(0)";
-                        points[2].style.msTransform = "scaleX(1) translateY(0)";
-                        points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-                    };
-                    
-                    revealFirstPoint();
-                    revealSecondPoint();
-                    revealThirdPoint();
-                    
-                    };
+     var points = document.getElementsByClassName('point');
+      
+     var revealPoint = function(pointNum) {
+         points[pointNum].style.opacity = 1;
+         points[pointNum].style.transform = "scaleX(1) translateY(0)";
+         points[pointNum].style.msTransform = "scaleX(1) translateY(0)";
+         points[pointNum].style.WebkitTransform = "scaleX(1) translateY(0)";
+     };
+      for(var i = 0; i < points.length; i++){
+          revealPoint(i);
+      }
+  };
