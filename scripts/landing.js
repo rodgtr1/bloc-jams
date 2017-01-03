18a71e27ed0fc2be160e8317ee8c01defd283b6d
Travis Rodgers <rodgtr1@gmail.com>
2017-01-03 15:36:34 -0500

    checkpoint9DOMScriptingEvents: Complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-  var animatePoints = function() {
- 
-     var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points){
       
      var revealPoint = function(pointNum) {
          points[pointNum].style.opacity = 1;
@@ -12,3 +12,17 @@
           revealPoint(i);
       }
   };
+
+window.onload = function() {
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event){
+        if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
+            animatePoints(pointsArray);
+        }
+    });
+}
