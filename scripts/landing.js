67b9c6af2eb2a21cc80dd469022cf41a6e70ef70
Travis Rodgers <rodgtr1@gmail.com>
2017-01-03 21:57:36 -0500

    checkpoint9DOMScriptingEvents2: Complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,27 +1,26 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points){
-      
-     var revealPoint = function(pointNum) {
-         points[pointNum].style.opacity = 1;
-         points[pointNum].style.transform = "scaleX(1) translateY(0)";
-         points[pointNum].style.msTransform = "scaleX(1) translateY(0)";
-         points[pointNum].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
-      for(var i = 0; i < points.length; i++){
-          revealPoint(i);
-      }
-  };
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+
+var animatePoints = function(points) {
+    forEach(points, revealPoint);
+};
 
 window.onload = function() {
     if (window.innerHeight > 950) {
         animatePoints(pointsArray);
     }
+    
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
     
-    window.addEventListener('scroll', function(event){
-        if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
+    window.addEventListener("scroll", function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
             animatePoints(pointsArray);
         }
     });
