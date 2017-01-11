feb8e2b1cd8f20439579bdc372bb19d81ef8f394
Travis Rodgers <rodgtr1@gmail.com>
2017-01-11 16:59:59 -0500

    checkpoint17JQueryCollectionAndAlbum: Complete

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,5 +1,6 @@
-var collectionItemTemplate =
-     '<div class="collection-album-container column fourth">'
+var buildCollectionItemTemplate = function() {
+     var template =
+    '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
    + '    <p>'
@@ -13,14 +14,18 @@ var collectionItemTemplate =
    + '  </div>'
    + '</div>'
    ;
+    
+    return $(template);
+ };
 
- window.onload = function() {
+$(window).load(function() {
 
-     var collectionContainer = document.getElementsByClassName('album-covers')[0];
+    var $collectionContainer = $('.album-covers');
 
-     collectionContainer.innerHTML = '';
+    $collectionContainer.empty();
  
      for (var i = 0; i < 12; i++) {
-         collectionContainer.innerHTML += collectionItemTemplate;
+         var $newThumbnail = buildCollectionItemTemplate();
+         $collectionContainer.append($newThumbnail);     
      }
- }
+ });
