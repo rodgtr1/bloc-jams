1599e5aaa65989de5e076ba4e7f6edcf31889369
Travis Rodgers <rodgtr1@gmail.com>
2017-01-13 20:39:39 -0500

    Blue audioUrl misspelled caused bug

--- scripts/fixtures.js
+++ scripts/fixtures.js
@@ -5,11 +5,11 @@ var albumPicasso = {
      year: '1881',
      albumArtUrl: 'assets/images/album_covers/01.png',
      songs: [
-         { title: 'Blue', duration: '4:26', audioURL: 'assets/music/blue' },
+         { title: 'Blue', duration: '4:26', audioUrl: 'assets/music/blue' },
          { title: 'Green', duration: '3:14', audioUrl: 'assets/music/green' },
          { title: 'Red', duration: '5:01', audioUrl: 'assets/music/red' },
-         { title: 'Pink', duration: '3:21', audioUrl: 'assets/music/pink'},
-         { title: 'Magenta', duration: '2:15', audioUrl: 'assets/music/magenta'}
+         { title: 'Pink', duration: '3:21', audioUrl: 'assets/music/pink' },
+         { title: 'Magenta', duration: '2:15', audioUrl: 'assets/music/magenta' }
      ]
  };
 
