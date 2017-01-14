c67ea4bbdbfe81dffeaa28407e624c11947bacfe
Travis Rodgers <rodgtr1@gmail.com>
2017-01-13 20:23:22 -0500

    checkpoint20JQueryBuzzComplete

--- scripts/album.js
+++ scripts/album.js
@@ -1,6 +1,23 @@
 var setSong = function(songNumber){
+    if (currentSoundFile) {
+        currentSoundFile.stop();
+    }
+    
     currentlyPlayingSongNumber = parseInt(songNumber);
     currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+    
+    currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+         formats: [ 'mp3' ],
+         preload: true
+     });
+    
+    setVolume(currentVolume);
+ };
+
+var setVolume = function(volume) {
+    if (currentSoundFile) {
+        currentSoundFile.setVolume(volume);
+    }
 }
 
 var getSongNumberCell = function(number) {
@@ -26,14 +43,21 @@ var createSongRow = function(songNumber, songName, songLength) {
 		currentlyPlayingCell.html(currentlyPlayingSongNumber);
 	}
 	if (currentlyPlayingSongNumber !== songNumber) {
-		$(this).html(pauseButtonTemplate);
 		setSong(songNumber);
+        currentSoundFile.play();
+		$(this).html(pauseButtonTemplate);
+        currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
         updatePlayerBarSong();
 	} else if (currentlyPlayingSongNumber === songNumber) {
-		$(this).html(playButtonTemplate);
-        $('.main-controls .play-pause').html(playerBarPlayButton);
-		currentlyPlayingSongNumber = null;
-        currentSongFromAlbum = null;
+        if (currentSoundFile.isPaused()) {
+            $(this).html(pauseButtonTemplate);
+            $('.main-controls .play-pause').html(playerBarPauseButton);
+            currentSoundFile.play();
+        } else {
+            $(this).html(playButtonTemplate);
+            $('.main-controls .play-pause').html(playerBarPlayButton);
+            currentSoundFile.pause();   
+        }
 	}
 };
     
@@ -105,6 +129,8 @@ var nextSong = function() {
     
     // Set a new current song
     setSong(currentSongIndex + 1);
+    currentSoundFile.play();
+
 
     // Update Player Bar information
     $('.currently-playing .song-name').text(currentSongFromAlbum.title);
@@ -137,6 +163,8 @@ var previousSong = function() {
     
     // Set a new current song
     setSong(currentSongIndex + 1);
+    currentSoundFile.play();
+
 
     // Update Player Bar information
     $('.currently-playing .song-name').text(currentSongFromAlbum.title);
@@ -168,6 +196,8 @@ var playerBarPauseButton = '<span class="ion-pause"></span>';
 var currentAlbum = null;
 var currentlyPlayingSongNumber = null;
 var currentSongFromAlbum = null;
+var currentSoundFile = null;
+var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
